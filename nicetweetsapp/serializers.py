a4afce6e551b3a7d82153922c7e938714dd35715
Lewis Gentle <lewis.gentle@gmail.com>
2022-01-04 19:40:36 +1100

    Change datetime serialisation to integer

--- nicetweetsapp/serializers.py
+++ nicetweetsapp/serializers.py
@@ -1,6 +1,15 @@
 from rest_framework import serializers
 from nicetweetsapp.models import Tweet
+import pandas as pd
+import time
 
+# for converting timestamp to an integer and vice versa
+class TimestampField(serializers.Field):
+    def to_representation(self, value):
+        return int(time.mktime(value.timetuple())*1000)
+
+    def to_internal_value(self, data):
+        return pd.to_datetime(data)
 
 # serialiser for the tweet model to be used in the api
 # serialiser converts the model into a json object
@@ -8,8 +17,9 @@ class TweetSerializer(serializers.ModelSerializer):
 
     tweet_text = serializers.CharField(source='text')
     tweet_sentiment = serializers.FloatField(source='sentiment')
-    tweet_created_at = serializers.DateTimeField(source='created_at')
+    tweet_created_at = TimestampField(source='created_at')
     tweet_topic = serializers.CharField(source='topic')
+
 
     class Meta:
         model = Tweet
